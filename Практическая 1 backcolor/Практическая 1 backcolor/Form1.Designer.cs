d12486e0727c4dfbd2fd8996a48b694390e84d0f
barinskaya <114239444+barinskaya@users.noreply.github.com>
2023-02-04 10:04:03 +0300

    Add files via upload

---
+++ Практическая 1 backcolor/Практическая 1 backcolor/Form1.Designer.cs
@@ -0,0 +1,89 @@
+﻿
+namespace Практическая_1_backcolor
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnExit = new System.Windows.Forms.Button();
+            this.txt = new System.Windows.Forms.TextBox();
+            this.lst = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(245, 128);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(75, 23);
+            this.btnExit.TabIndex = 0;
+            this.btnExit.Text = "Выход";
+            this.btnExit.UseVisualStyleBackColor = true;
+            // 
+            // txt
+            // 
+            this.txt.Location = new System.Drawing.Point(12, 128);
+            this.txt.Name = "txt";
+            this.txt.Size = new System.Drawing.Size(111, 20);
+            this.txt.TabIndex = 1;
+            // 
+            // lst
+            // 
+            this.lst.FormattingEnabled = true;
+            this.lst.Items.AddRange(new object[] {
+            "черный",
+            "красный",
+            "зеленый",
+            "синий"});
+            this.lst.Location = new System.Drawing.Point(12, 12);
+            this.lst.Name = "lst";
+            this.lst.Size = new System.Drawing.Size(120, 95);
+            this.lst.TabIndex = 2;
+            this.lst.SelectedIndexChanged += new System.EventHandler(this.lst_SelectedIndexChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(341, 177);
+            this.Controls.Add(this.lst);
+            this.Controls.Add(this.txt);
+            this.Controls.Add(this.btnExit);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.TextBox txt;
+        private System.Windows.Forms.ListBox lst;
+    }
+}
+
---
+++ Практическая 1 backcolor/Практическая 1 backcolor/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Практическая_1_backcolor
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void lst_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (lst.SelectedItem == "black")
+            {
+                txt.BackColor = System.Drawing.Color.Black;
+            }
+            else if (lst.SelectedItem == "red")
+            {
+                txt.BackColor = System.Drawing.Color.Red;
+            }
+            else if (lst.SelectedItem == "blue")
+            {
+                txt.BackColor = System.Drawing.Color.Blue;
+            }
+            else if (lst.SelectedItem == "green")
+            {
+                txt.BackColor = System.Drawing.Color.Green;
+            }
+        }
+    }
+}
